da8b4433b918e4d35507c455b6416364ad3be19d
mosidrum <mosiokanga@gmail.com>
2024-11-11 21:36:33 +0100

    add dashboard header

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,26 @@
 import './Dashboard.scss';
 import { Outlet } from 'react-router-dom';
+import { IoIosNotificationsOutline, IoMdArrowDropdown } from 'react-icons/io';
+
 import { Logo, SearchBox } from '../../components';
+import user from '../../assets/user.png';
 
 export const Dashboard = () => (
   <div className="dashboard-layout">
-    <header className="header d-flex align-items-center">
-      <Logo />
-      <SearchBox />
+    <header className="header d-flex justify-content-between align-items-center">
+      <div className="d-flex justify-content-between width-50 align-items-center">
+        <Logo />
+        <SearchBox />
+      </div>
+      <div className="d-flex justify-content-end gap-2 align-items-center  width-50">
+        <div className="text-color-secondary text-underline mr-2 cursor-pointer">Docs</div>
+        <IoIosNotificationsOutline color="#213f7d" size={26} />
+        <img src={user} alt="user" className="user" />
+        <div className="text-color-secondary">Adedeji</div>
+        <IoMdArrowDropdown color="#213f7d" size={18} className="ml-n1" />
+      </div>
     </header>
-    <aside className="sidebar">Sidebar</aside>
+    <aside className="sidebar background-white">Sidebar</aside>
     <main className="content">
       <Outlet />
     </main>
