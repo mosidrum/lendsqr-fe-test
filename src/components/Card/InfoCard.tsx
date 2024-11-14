a00bd1ce3c436144132d3b6ec83999b76e6a8f53
mosidrum <mosiokanga@gmail.com>
2024-11-14 04:13:15 +0100

    fix user card and section

--- src/components/Card/InfoCard.tsx
+++ src/components/Card/InfoCard.tsx
@@ -1,10 +1,10 @@
 type InfoCardProps = {
   label: string;
-  data: string;
+  data: string | number;
 };
 export const InfoCard = ({ label, data }: InfoCardProps) => {
   return (
-    <div className="d-flex flex-dir-column gap-1 width-fit-content">
+    <div className="d-flex flex-dir-column gap-1 width-fit-content mx-3">
       <div className="font-small text-color-primary">{label}</div>
       <div className="font-normal text-color-primary fw-normal">{data}</div>
     </div>
--- src/pages/Users/InfoSection.tsx
+++ src/pages/Users/InfoSection.tsx
@@ -4,15 +4,15 @@ export type InfoSectionProps = {
   sectionTitle: string;
   data?: {
     label: string;
-    data: string;
+    data: string | number;
   }[];
 };
 export const InfoSection = ({ sectionTitle, data }: InfoSectionProps) => {
   return (
     <div className="width-100">
       <div className="font-large text-color-secondary fw-normal mb-3">{sectionTitle}</div>
-      <div className="d-flex wrap">
-        {data?.map((item) => <InfoCard label={item.label} data={item.data} />)}
+      <div className="d-flex gap-5 ml-n3 wrap">
+        {data?.map((item, index) => <InfoCard key={index} label={item.label} data={item.data} />)}
       </div>
     </div>
   );
